e9c3d1c9e204007a79cd4bb9f25eef4f0e286932
kerlw <kerl.d.w@gmail.com>
2016-01-13 19:07:43 +0800

    Add. Use socket.io implements connection & communication. Use index.html as the main UI

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var jqy = require('jquery');
+var path = require('path');
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    console.log('__dirname = ' + __dirname);
+    res.sendFile(path.join(__dirname,'/index.html'));
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
