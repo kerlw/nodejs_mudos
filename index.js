dfd1d3f17b05aa8a547b261a9cfab6fd2b3a0a6a
zhuww <zhuww@jixunsoft.cn>
2016-01-27 19:32:36 +0800

    更换页面

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/public', express.static(__dirname + '/public'));
 //io.set('heartbeats interval', 20);
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'/index.html'));
+    res.sendFile(path.join(__dirname,'/index.htm'));
 });
 
 io.on('connection', function(socket) {
